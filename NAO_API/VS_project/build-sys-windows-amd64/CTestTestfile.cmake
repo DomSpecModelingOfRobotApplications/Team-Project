2f59ec1ff00525e586b84b12bf28f28d33c8fd51
Mariia <zryaninam@gmail.com>
2014-02-14 16:31:56 +0100

    delete build folder helloworld (from examples)

--- NAO_API/VS_project/build-sys-windows-amd64/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: d:/studies Mannheim/My Team Project/GIT/TeamProject-master/Team-Project/NAO_API/VS_project
-# Build directory: D:/studies Mannheim/My Team Project/GIT/TeamProject-master/Team-Project/NAO_API/VS_project/build-sys-windows-amd64
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- NAO_API/VS_project/main.cpp
+++ NAO_API/VS_project/main.cpp
@@ -1,12 +1,54 @@
-/*
- * Copyright (c) 2012, 2013 Aldebaran Robotics. All rights reserved.
- * Use of this source code is governed by a BSD-style license that can be
- * found in the COPYING file.
+/**
+ * Copyright (c) 2010 Aldebaran Robotics
  */
-#include <iostream>
 
-int main()
+#ifndef _WIN32
+# include <signal.h>
+#endif
+
+#include <alcommon/albroker.h>
+#include <alcommon/albrokermanager.h>
+#include <alcommon/altoolsmain.h>
+#include "helloworld.h"
+
+#ifdef HELLOWORLD_IS_REMOTE
+# define ALCALL
+#else
+// when not remote, we're in a dll, so export the entry point
+# ifdef _WIN32
+#  define ALCALL __declspec(dllexport)
+# else
+#  define ALCALL
+# endif
+#endif
+
+extern "C"
 {
-  std::cout << "Hello, world" << std::endl;
-  return 0;
-}
+  ALCALL int _createModule(boost::shared_ptr<AL::ALBroker> pBroker)
+  {
+    // init broker with the main broker instance
+    // from the parent executable
+    AL::ALBrokerManager::setInstance(pBroker->fBrokerManager.lock());
+    AL::ALBrokerManager::getInstance()->addBroker(pBroker);
+    // create module instances
+    AL::ALModule::createModule<HelloWorld>(pBroker, "HelloWorld");
+    return 0;
+  }
+
+  ALCALL int _closeModule(  )
+  {
+    return 0;
+  }
+} // extern "C"
+
+
+#ifdef HELLOWORLD_IS_REMOTE
+  int main(int argc, char *argv[])
+  {
+    // pointer to createModule
+    TMainType sig;
+    sig = &_createModule;
+    // call main
+    return ALTools::mainFunction("HelloWorld", argc, argv, sig);
+  }
+#endif
